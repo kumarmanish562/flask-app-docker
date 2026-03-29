ddb142ebafb940fcf1579b16c5ad3801bc387f9a
kumarmanish562 <qmanishkumar562@gmail.com>
2026-03-29 11:19:01 +0530

    update hello_world route to return a welcome message instead of rendering a template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return 'Hey , welcome to my Flask app running in Docker!'
 
 
 @app.route('/health')
